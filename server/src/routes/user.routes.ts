8cdf0ada7215e8c5e2c4b12a6a9796155c3a3a66
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-06-09 17:31:52 +0630

    refactor: user cotroller

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import { isOwner, verifyUserToken } from '../middlewares/authenticate';
 import { UsersController } from '../controllers/user.controller';
-const usersController = new UsersController();
 //routes are private
 export default (router: express.Router) => {
-  router.get('/users', usersController.index);
-  router.get('/users/:id', verifyUserToken, usersController.show);
-  router.delete('/users/:id', verifyUserToken, isOwner, usersController.delete);
-  router.put('/users/:id', verifyUserToken, isOwner, usersController.update);
+  router.get('/users', UsersController.index);
+  router.get('/users/:id', verifyUserToken, UsersController.show);
+  router.delete('/users/:id', verifyUserToken, isOwner, UsersController.delete);
+  router.put('/users/:id', verifyUserToken, isOwner, UsersController.update);
 };
