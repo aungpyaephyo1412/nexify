707ead908c20e8e4b309b1162740fb58e9e1424d
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-06-10 19:57:01 +0630

    feat: user update

--- server/src/controllers/post.controller.ts
+++ server/src/controllers/post.controller.ts
@@ -4,9 +4,15 @@ import { PostModel } from '../models/post.service';
 export const PostsController = {
   async index(_: Request, res: Response) {
     try {
-      const data = await PostModel.find().populate({ path : "user" , select : "-password -notifications -stories -messages -rememberToken -expiredToken -resetToken"}).sort({
-        createdAt : "descending"
-      })
+      const data = await PostModel.find()
+        .populate({
+          path: 'user',
+          select:
+            '-password -notifications -stories -messages -rememberToken -expiredToken -resetToken',
+        })
+        .sort({
+          createdAt: 'descending',
+        });
       return res.status(200).json({ data });
     } catch (error) {
       console.log(error);
@@ -16,31 +22,30 @@ export const PostsController = {
 
   async show(req: Request, res: Response) {
     const { id } = req.params;
-    const data = await PostModel.findOne({
-      username: id
-    }).select(
-      '-password -stories -notifications -messages -rememberToken -expiredToken -resetToken'
-    );
+    const data = await PostModel.findById(id).populate({
+      path: 'user',
+      select:
+        '-password -notifications -stories -messages -rememberToken -expiredToken -resetToken',
+    });
     if (!data) return res.status(404).json({ error: 'User not found!' });
-    return res.status(200).json({ data });
+    return res.status(200).json(data);
   },
-  async store(req : Request,res : Response){
-    try{
-      const data = await PostModel.create(req.body)
+  async store(req: Request, res: Response) {
+    try {
+      const data = await PostModel.create(req.body);
       console.log(data);
-      return res.status(201).json({message : "Post created"})
-    }catch (e){
+      return res.status(201).json({ message: 'Post created' });
+    } catch (e) {
       console.log(e);
       return res.status(400).json({ message: 'Something went wrong!' });
-
     }
   },
   async update(req: Request, res: Response) {
     const { id } = req.params;
-    const user = await PostModel.findById(id).select('-password');
-    if (!user)
+    const post = await PostModel.findById(id).select('-password');
+    if (!post)
       return res.status(400).json({ message: 'Something went wrong!' });
-    await user.save();
+    await post.save();
     return res.status(200).json({ message: 'Update user successfully!' });
   },
 
