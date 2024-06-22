00d422bc2f8a14a0ec1b661d78fa184f9e22631d
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-06-22 18:56:25 +0630

    fix: user post layout;

--- client/app/(user)/[slug]/page.tsx
+++ client/app/(user)/[slug]/page.tsx
@@ -6,12 +6,10 @@ import { useParams } from 'next/navigation';
 const Page = () => {
   const { slug } = useParams();
   return (
-    <div className="">
-      <div className="w-full flex-1 ">
-        <TabsContent value={'posts'} className="w-full pb-[35px] space-y-5 screen-padding">
-          <UserPosts userId={slug as string} />
-        </TabsContent>
-      </div>
+    <div className="w-full flex-1 ">
+      <TabsContent value={'posts'} className="w-full pb-[35px] space-y-5 screen-padding">
+        <UserPosts userId={slug as string} />
+      </TabsContent>
     </div>
   );
 };
